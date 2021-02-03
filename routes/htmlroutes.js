0dc9b3f723fe281164c035ef5f69bc106057cfb3
Matthew Marchand <marchandmr@icloud.com>
2021-02-02 21:25:16 -0600

    added login.js with passport

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router();
-//var user = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-
     res.render("login");
 });
 
+module.exports = router;
