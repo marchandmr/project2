7b9c52d675c4cadd64afae7b59e8dc5752d8740c
Luis Vela <luismvela1@aol.com>
2021-02-02 19:05:25 -0600

    added html routes to the routs folder

--- routes/active_prize_routes.js
+++ routes/active_prize_routes.js
@@ -1,6 +1,9 @@
 var db = require("../models");
 
-
 module.exports = function(app){
-
-}
+    app.get("/",function(req,res){
+        db.Prizes.findAll({}).then(function(dbPrizes){
+            res.json(dbPrizes);
+        });
+    });
+    };
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,8 +1,33 @@
 var path = require("path");
 
 
-module.exports = function(app){
-    app.get("",function(req,res){
-        
-    })
-}
+
+module.exports = function (app) {
+    // index route loads 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/index.handlebars"));
+    });
+
+    // cms route loads cms.html
+    app.get("/chores", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/chores.handlebars"));
+    });
+
+    // blog route loads blog.html
+    app.get("/createAcct", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/createAcct.handlebars"));
+    });
+
+    // authors route loads author-manager.html
+    app.get("/main", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/main.handlebars"));
+    });
+
+    app.get("/login", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/login.handlebars"));
+    });
+
+    app.get("/prize", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/prize.handlebars"));
+    });
+};
