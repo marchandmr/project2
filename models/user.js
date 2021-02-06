2314237b97d63e940412ef6cafc5c6a793d4ec97
Matthew Marchand <marchandmr@icloud.com>
2021-02-06 10:46:58 -0600

    updating routes

--- models/user.js
+++ models/user.js
@@ -23,13 +23,6 @@ module.exports = function (sequelize, DataTypes) {
         }
     });
 
-    user.associate = function (models) {
-        user.belongsTo(models.user, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     user.prototype.validPassword = function (password) {
         return bcrypt.compareSync(password, this.password);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,66 +1,85 @@
 var db = require("../models");
-var router = express.Router();
 
 
 
-//routes to find all chores
-router.get("/api/chores", function(req, res) {
-  db.Chores.findAll({}).then(function(dbChores) {
-    res.json(dbChores);
+module.exports = function (app) {
+  //routes to find all chores
+  app.get("/api/chores", function (req, res) {
+    db.Chores.findAll({}).then(function (dbChores) {
+      res.json(dbChores);
+    });
   });
-});
 
-// routes to create a chore
-router.post("/api/chores", function(req, res) {
-  console.log(req.body.chore_Name);
-  db.Chores.insertOne(req.body.chore_Name).then(function(dbChores) {
-    res.json(dbChores);
+  // routes to create a chore
+  app.post("/api/chores", function (req, res) {
+    console.log(req.body.chore_Name);
+    db.Chores.insertOne(req.body.chore_Name).then(function (dbChores) {
+      res.json(dbChores);
+    });
   });
-});
 
-//routes to create new kid
-router.post("/api/user", function(req, res) {
-  console.log(req.body.kid_name);
-  db.User.insertOne(req.body.kid_name).then(function(dbUser) {
-    res.json(dbUser);
+  //routes to create new kid
+  app.post("/api/user", function (req, res) {
+    console.log(req.body.kid_name);
+    db.User.insertOne(req.body.kid_name).then(function (dbUser) {
+      res.json(dbUser);
+    });
   });
-});
-//routes to find all kids
-router.get("/api/kids", function(req, res) {
-  db.Kids.findAll({}).then(function(dbKids) {
-    res.json(dbKids);
+  //routes to find all kids
+  app.get("/api/kids", function (req, res) {
+    db.Kids.findAll({}).then(function (dbKids) {
+      res.json(dbKids);
+    });
   });
-});
 
-//routes to find a family user
-router.get("/api/user/:id", function(req, res) {
-  db.User.findOne({
-    where: {
-      id: req.params.id
-    }
-  }).then(function(dbUser) {
-    res.json(dbUser);
+  //routes to find a family user
+  app.get("/api/user/:id", function (req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbUser) {
+      res.json(dbUser);
+    });
   });
-});
 
-//route to claim a chore
-router.post('/chores/claim/:id', function (req, res) {
-  db.Chores.updateOne(req.params.id, function() {
-    res.redirect('/index');
+  //route to claim a chore
+  app.post('/chores/claim/:id', function (req, res) {
+    db.Chores.updateOne(req.params.id, function () {
+      res.redirect('/index');
+    });
   });
-});
 
-//routes to find all prizes
-router.get("/api/prize", function(req, res) {
-  db.Prize.findAll({}).then(function(dbPrize) {
-    res.json(dbPrize);
+  //routes to find all prizes
+  app.get("/api/prize", function (req, res) {
+    db.Prize.findAll({}).then(function (dbPrize) {
+      res.json(dbPrize);
+    });
   });
-});
 
-// routes to create a prize
-router.post("/api/prize", function(req, res) {
-  console.log(req.body.prize);
-  db.Chores.insertOne(req.body.prize).then(function(dbPrize) {
-    res.json(dbPrize);
+  // routes to create a prize
+  app.post("/api/prize", function (req, res) {
+    console.log(req.body.prize);
+    db.Chores.insertOne(req.body.prize).then(function (dbPrize) {
+      res.json(dbPrize);
+    });
   });
-});
+
+  //route to delete a chore
+  app.delete("/api/chores/:id", function (req, res) {
+    db.Chores.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbChores) {
+      res.json(dbChores);
+    });
+  });
+
+  //routes to complete a chore
+  app.post('/chores/complete/:id', function (req, res) {
+    db.Chores.updateOne(req.params.id, function () {
+      res.redirect('/index');
+    });
+  });
+};
